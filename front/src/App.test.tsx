c85a59bf99227476bd13b1318f280faf88cb296d
Leired7 <leirediez.7@gmail.com>
2021-10-26 19:03:48 +0200

    Show json gifs

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import gifs from './gif.json';
 
 describe('Show 50 gifs', () => {
   it('show error when no gifs', () => {
     render(<App />);
-    const errorElement = screen.getByText(/No se han podido mostrar los gifs./i);
+    const errorElement = screen.getByText(
+      /No se han podido mostrar los gifs./i
+    );
     expect(errorElement).toBeInTheDocument();
   });
 
-  it('show 2 gifs', () => {
+  it('show json gifs', () => {
     render(<App />);
-    const gifElement = screen.getAllByRole('img');
-    const xboxElement = gifElement[0]
-    const eatnparkElement = gifElement[1]
-    expect(xboxElement).toHaveAttribute('alt', 'xbox-retro-tape-mixtape');
-    expect(eatnparkElement).toHaveAttribute('alt', 'eatnpark');
+    const gifElements = screen.getAllByRole('img');
+
+    for (let i = 0; i < gifs.length; i++) {
+      expect(gifElements[i]).toHaveAttribute('alt', gifs[i].title);
+    }
   });
-})
+});
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
+import gifs from './gif.json';
 
 function App() {
   return (
     <>
-      <div>
-        No se han podido mostrar los gifs.
-      </div>
-      <img alt="xbox-retro-tape-mixtape" src="https://giphy.com/gifs/xbox-retro-tape-mixtape-AZAAOH30oWEml9QB5E"/>
-      <img alt="eatnpark" src="https://giphy.com/gifs/eatnpark-Xw3pnKSKhcH7ozPozR"/>
+      <div>No se han podido mostrar los gifs.</div>
+      {gifs.map((gif, index) => (
+        <img key={index} alt={gif.title} src={gif.url} />
+      ))}
     </>
   );
 }
