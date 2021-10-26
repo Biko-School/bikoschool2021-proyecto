1705e5e7b7926776a091ae28cca5728ee24b8874
Yaiza Alvarez <yaialvarez13@gmail.com>
2021-10-26 20:32:11 +0200

    Añadir test de la lista de memes

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import memes from './memes.json'
 
 describe ('App', () => {
-  it('get Hello World', () => {
+
+  it('found memes list', () => {
     render (<App />)
-    expect (screen.getByText("Hello World!")).toBeInTheDocument()
+    for (let meme of memes.results){    
+      expect (screen.getByText(meme.title)).toBeInTheDocument()
+    }
   })
+
+
 })
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import memes from './memes.json'
 
 function App() {
+  
+  // const [memes, setMemes] = useState([])
+
+  // const fechMemes = () => { 
+  //   setMemes(memesJson)
+  // }
+
+  // useEffect(() => {
+  //   fechMemes()
+  // }, []);
+
+  const memesList = memes.results.map((meme) =>
+    <li key={meme.title}>{meme.title}</li>
+  );
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ul>
+        {memesList}
+      </ul>
     </div>
-  );
+  );  
 }
 
 export default App;
