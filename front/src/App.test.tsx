1804227fb6b414516af7f21e2ccbe85946643b1e
Endika <endikaorve@gmail.com>
2021-10-26 18:49:51 +0200

    Show 2 gifs

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('show error when no gifs', () => {
-  render(<App />);
-  const errorElement = screen.getByText(/No se han podido mostrar los gifs./i);
-  expect(errorElement).toBeInTheDocument();
-});
+describe('Show 50 gifs', () => {
+  it('show error when no gifs', () => {
+    render(<App />);
+    const errorElement = screen.getByText(/No se han podido mostrar los gifs./i);
+    expect(errorElement).toBeInTheDocument();
+  });
+
+  it('show 2 gifs', () => {
+    render(<App />);
+    const gifElement = screen.getAllByRole('img');
+    const xboxElement = gifElement[0]
+    const eatnparkElement = gifElement[1]
+    expect(xboxElement).toHaveAttribute('alt', 'xbox-retro-tape-mixtape');
+    expect(eatnparkElement).toHaveAttribute('alt', 'eatnpark');
+  });
+})
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      No se han podido mostrar los gifs.
-    </div>
+    <>
+      <div>
+        No se han podido mostrar los gifs.
+      </div>
+      <img alt="xbox-retro-tape-mixtape" src="https://giphy.com/gifs/xbox-retro-tape-mixtape-AZAAOH30oWEml9QB5E"/>
+      <img alt="eatnpark" src="https://giphy.com/gifs/eatnpark-Xw3pnKSKhcH7ozPozR"/>
+    </>
   );
 }
 
