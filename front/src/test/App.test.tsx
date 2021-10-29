be37ff990cf1f452a172e413c4ac13bcd3894e96
Javier Rodríguez <javitafalla95@gmail.com>
2021-10-29 20:26:11 +0200

    test will show 3 gif

--- front/src/test/App.test.tsx
+++ front/src/test/App.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import memesData from "./mockApi.json";
 
 describe("Home", () => {
   it("will show a gif", async () => {
@@ -8,5 +9,12 @@ describe("Home", () => {
 
     const display = await screen.findByRole("img", { name: "Kid punch" });
     expect(display).toBeInTheDocument();
+  });
+  it("will show 3 gif", async () => {
+    render(<App />);
+    memesData.results.map(async (meme) => {
+      const display = await screen.findByRole("img", { name: meme.title });
+      expect(display).toBeInTheDocument();
+    });
   });
 });
