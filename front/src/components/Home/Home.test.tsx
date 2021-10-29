54cd19505f0c657a2433953f793c8be20c8c2a53
Endika <endikaorve@gmail.com>
2021-10-29 20:06:55 +0200

    Added mock service to handle error in test

--- front/src/components/Home/Home.test.tsx
+++ front/src/components/Home/Home.test.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { Home } from './Home';
+import { render, screen } from '@testing-library/react';
+import {rest} from 'msw'
 import gifs from './../../gif.json';
+import { server } from '../../mocks/server';
 
 describe('show 50 gifs', () => {
-  it('show error when no gifs', () => {
+
+  it('show error when no gifs', async () => {
+
+    server.use(
+      rest.get('http://api.bikoschool.dev/gifs', (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({error: 'Server Error'}));
+      })
+    )
+  
     render(<Home />);
-    const errorElement = screen.getByText(
+    const errorElement = await screen.findByText(
       /No se han podido mostrar los gifs./i
     );
 
