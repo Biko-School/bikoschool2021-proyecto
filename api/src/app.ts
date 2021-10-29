d4629a1931fd1beb62acaf19bdf430d1e4c2d9dc
Carlos Martinez <carlos.martinez@biko2.com>
2021-10-29 19:05:58 +0200

    Refactor: propaga bd a rutas a través de contexto de express

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import morgan from "morgan";
 import low from "lowdb";
 
-import { createRoutes } from "./routes";
+import { routes } from "./routes";
 import { DatabaseSchema } from "./DatabaseSchema";
 
 export function createApp(db: low.LowdbSync<DatabaseSchema>) {
@@ -11,6 +11,13 @@ export function createApp(db: low.LowdbSync<DatabaseSchema>) {
   // Shows request log on terminal
   // https://github.com/expressjs/morgan
   app.use(morgan("dev"));
+
+  // Añade el adaptador de la base de datos en cada petición
+  app.use((req, res, next) => {
+    req.context = { db };
+    next();
+  });
+
   // Parses incoming requests with JSON payloads
   // http://expressjs.com/es/api.html#express.json
   app.use(express.json());
@@ -18,7 +25,7 @@ export function createApp(db: low.LowdbSync<DatabaseSchema>) {
   // http://expressjs.com/es/api.html#express.urlencoded
   app.use(express.urlencoded({ extended: false }));
 
-  app.use("/api", createRoutes(db));
+  app.use("/api", routes);
 
   return app;
 }
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,15 +1,10 @@
 import { Router } from "express";
-import low from "lowdb";
-import { DatabaseSchema } from "./DatabaseSchema";
 
-export function createRoutes(db: low.LowdbSync<DatabaseSchema>) {
-  const routes = Router();
+export const routes = Router();
 
-  routes.get("/memes", (req, res) => {
-    const memes = db.get("memes").take(50).value();
+routes.get("/memes", (req, res) => {
+  const db = req.context.db;
+  const memes = db.get("memes").take(50).value();
 
-    res.status(200).json(memes);
-  });
-
-  return routes;
-}
+  res.status(200).json(memes);
+});
