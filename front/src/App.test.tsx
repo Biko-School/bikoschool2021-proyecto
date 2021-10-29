f27c4eed7101318221907acb6850e969a9a8ed7c
GiTurki <turkidzh@gmail.com>
2021-10-29 18:45:26 +0200

    Unifica tests

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -9,14 +9,6 @@ test("Muestra el meme Happy Birthday", () => {
   });
 
   expect(memeItem).toBeInTheDocument();
-});
-
-test("Muestra la imagen del meme Happy Birthday", () => {
-  render(<App />);
-  const memeItem = screen.getByRole("img", {
-    name: "Happy Birthday",
-  });
-
   expect(memeItem).toHaveAttribute(
     "src",
     "https://i.giphy.com/media/H3kNUxf6NubwitJuu6/giphy.webp"
