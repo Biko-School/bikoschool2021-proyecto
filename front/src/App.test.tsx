6e3c02feb489ed22856a92228742f60039eb417e
GiTurki <turkidzh@gmail.com>
2021-10-29 17:57:45 +0200

    Muestra el meme Happy Birthday

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('must render a meme', () => {
-  render(<App alt={"Happy Birthday GIF by Kevin Ross"} src={""}/>);
-  const memeItem = screen.getByRole('img',{name: 'Happy Birthday GIF by Kevin Ross'});
+test("Muestra el meme Happy Birthday", () => {
+  render(<App />);
+  const memeItem = screen.getByRole("img", {
+    name: "Happy Birthday",
+  });
+
   expect(memeItem).toBeInTheDocument();
 });
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-interface Meme {
-  alt: string;
-  src: string;
-}
-
-function App(meme: Meme) {
-  return (
-   <img src={meme.src} alt={meme.alt}/>  );
+function App() {
+  return <img alt="Happy Birthday" />;
 }
 
 export default App;
