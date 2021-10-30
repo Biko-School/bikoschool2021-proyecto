5dacbcb6db8ccf83bc83b6b54e9213e91e58aeb7
rocio0504 <rocioarp2@gmail.com>
2021-10-30 10:37:43 +0200

    separate express in a new file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,5 @@
 import http, { Server } from "http";
-import express, { Express, NextFunction, Request, Response } from "express";
-import morgan from "morgan";
-
-const app: Express = express();
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(morgan("dev"));
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json());
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
+import { app } from './app'
 
 const port: string = process.env.PORT || "3000";
 
