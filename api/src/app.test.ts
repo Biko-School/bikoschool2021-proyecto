7a0e63ab7e32557fc6d3a67f9e027278c10cf587
rocio0504 <rocioarp2@gmail.com>
2021-10-30 12:43:04 +0200

    Test: devuelve un array de 50 memes

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -3,10 +3,11 @@ import request from 'supertest'
 import { response } from 'express'
 
 describe('/api/memes', () => {
-    it('existe el endpoint', (done) => {
+    it('recibe un array de longitud 50', (done) => {
         request(app).get('/api/memes').expect(200)
             .then((response) => {
                 expect(response.body).toBeInstanceOf(Array);
+                expect(response.body).toHaveLength(50);
                 done();
             })
     })
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 
 export const routes = Router();
 
+const memes = new Array(50)
+
 routes.get('/memes', function (req: Request, res: Response) {
-    res.status(200).send(['mundo', 'flor', 'piedra'])
+    res.status(200).send(memes)
 })
