4d7a892581b113c00451ff7d6c3a9cd704d45039
GiTurki <turkidzh@gmail.com>
2021-10-30 11:24:22 +0200

    Test: devuelve lista verde

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,7 +3,7 @@ import express, { Express, Request, Response, Router } from "express";
 const routes = express.Router();
 
 routes.get("/memes", (req: Request, res: Response) => {
-  res.sendStatus(200);
+  res.send([]);
 });
 
 export default routes;
