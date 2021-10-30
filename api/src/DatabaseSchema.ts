94f322a038b8a775c7804749c48fe92c87608052
Endika <endikaorve@gmail.com>
2021-10-30 14:05:27 +0200

    Refactor: Completed Meme Interface

--- api/src/DatabaseSchema.ts
+++ api/src/DatabaseSchema.ts
@@ -2,4 +2,33 @@ export interface DatabaseSchema {
   memes: Meme[];
 }
 
-interface Meme {}
+export interface Meme {
+  id: string;
+  type: string;
+  slug: string;
+  giphyUrl: string;
+  title: string;
+  source_tld: string;
+  source_post_url: string;
+  import_datetime: string;
+  username: string;
+  images: Images;
+  tags: string[];
+}
+
+export interface Images {
+  original: ImageOriginal;
+  small: ImageSmall;
+}
+
+export interface ImageOriginal {
+  width: string;
+  height: string;
+  url: string;
+}
+
+export interface ImageSmall {
+  width: string;
+  height: string;
+  url: string;
+}
