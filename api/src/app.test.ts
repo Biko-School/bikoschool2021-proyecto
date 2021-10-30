b14bab7e5f8db05f2074fbb0fc4dae7d42f5b6c2
GiTurki <turkidzh@gmail.com>
2021-10-30 11:22:54 +0200

    Test: devueleve lista la ruta /api/memes

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -5,4 +5,7 @@ describe("/api/memes", () => {
   it("existe el endpoint", (done) => {
     request(app).get("/api/memes").expect(200, done);
   });
+  it("Endpoint devuelve lista", (done) => {
+    request(app).get("/api/memes").expect([], done);
+  });
 });
