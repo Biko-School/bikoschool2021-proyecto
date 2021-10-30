a21a6bbb60320090b8c49596f935d0024d89c32c
Endika <endikaorve@gmail.com>
2021-10-30 12:17:16 +0200

    Test: Added API test 'endpoint returns array'

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -5,4 +5,14 @@ describe('/api/memes', () => {
   it('endpoint exists', (done) => {
     request(app).get('/api/memes').expect(200, done);
   });
+
+  it('endpoint returns array', (done) => {
+    request(app)
+      .get('/api/memes')
+      .end((err, res) => {
+        expect(Array.isArray(res.body)).toBeTruthy();
+        expect(res.body).toBeInstanceOf(Array);
+        done();
+      });
+  });
 });
