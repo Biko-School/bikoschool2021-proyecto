4bd72472bc9cbbc1f43ec30a62a3dbd5a82f91e7
Javier Rodríguez <javitafalla95@gmail.com>
2021-10-30 12:17:03 +0200

    test: should be array

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,8 +1,18 @@
 import { app } from "./app";
 import request from "supertest";
+import { response } from "express";
 
 describe("/api/memes", () => {
   it("Endpoint exist", (done) => {
     request(app).get("/api/memes").expect(200, done);
+  });
+  it("should be array", (done) => {
+    request(app)
+      .get("/api/memes")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeInstanceOf(Array);
+        done();
+      });
   });
 });
