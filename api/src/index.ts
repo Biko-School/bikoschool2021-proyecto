d466fc772ad3413225b8b97abf4dcba19bbf5c65
Maciker <ikermacaya@gmail.com>
2021-10-30 10:38:33 +0200

    fichero app creado - API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,28 +1,13 @@
-import http, { Server } from "http";
-import express, { Express, Request, Response } from "express";
-import morgan from "morgan";
+import http, { Server } from "http"
+import app from "./app"
 
-const app: Express = express();
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(morgan("dev"));
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json());
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
+const server: Server = http.createServer(app);
 
 const port: string = process.env.PORT || "3000";
 
 // Assigns setting name to value
 // http://expressjs.com/es/api.html#app.set
 app.set("port", port);
-
-const server: Server = http.createServer(app);
 
 server.listen(port);
 server.on("error", onError);
