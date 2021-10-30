e4e95650c9169861b4929d3596ed9bdfea078d85
Endika <endikaorve@gmail.com>
2021-10-30 10:20:47 +0200

    API Express Endopoint examples

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import http, { Server } from "http";
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import { nextTick } from "process";
 
 const app: Express = express();
 
@@ -16,10 +17,59 @@ app.use(express.json());
 // http://expressjs.com/es/api.html#express.urlencoded
 app.use(express.urlencoded({ extended: false }));
 
+//// ROUTES ////
+
+// Check session
+const checkSession = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (true) { // ## TODO: Check Session
+    console.log('checkSession')
+    next()
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.use("/users", checkSession)
+
+// Users CRUD
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).json([{ data: "Endika" },{ data: "Carlos" }]); // Status 200 default
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+  res.json(`User with ID ${req.params.id}: Endika`);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  console.log('Create user')
+  res.json(req.body);
+});
+
+app.put("/users", (req: Request, res: Response) => {
+  console.log('Update user')
+  res.json(req.body);
+});
+
+app.delete("/users", (req: Request, res: Response) => {
+  console.log('Delete user')
+  res.json(req.body);
+});
+
+app.get("/errors/users", (req: Request, res: Response, next: NextFunction) => {
+  next(new Error('Users Error'))
+});
+
 // Routes every path
 // http://expressjs.com/es/api.html#app.use
-app.use("/", (req: Request, res: Response) => {
-  res.json({ data: "index!" });
+//app.use("/", (req: Request, res: Response) => {
+//  res.json({ data: "index!" });
+//});
+
+// 404
+app.use('*', (req: Request, res: Response) => {
+  res.json('no-controller');
 });
 
 const port: string = process.env.PORT || "3000";
