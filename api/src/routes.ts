ffaa7a6131ad82fa5c9a6e07ada07308ccd905fe
GiTurki <turkidzh@gmail.com>
2021-10-30 13:42:09 +0200

    Chore: tipada la base de datos

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,43 @@
 import express, { Express, Request, Response, Router } from "express";
 import fs from "fs";
-import Meme from "meme";
+
+import low from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+
+interface DatabaseSchema {
+  memes: Meme[];
+}
+
+interface Meme {
+  id: string;
+  type: string;
+  slug: string;
+  giphyUrl: string;
+  title: string;
+  source_tld: string;
+  source_post_url: string;
+  import_datetime: Date;
+  username: string;
+  images: {
+    original: Image;
+    small: Image;
+  };
+  tags: string[];
+}
+
+interface Image {
+  width: number;
+  height: number;
+  url: string;
+}
+
+const adapter = new FileSync<DatabaseSchema>("./database/db.json");
+const db = low(adapter);
 
 const routes = express.Router();
 
 routes.get("/memes", (req: Request, res: Response) => {
-  res.send(new Array(50));
+  res.status(200).json(db.get("memes").take(50));
 });
 
 export default routes;
