c1a58127c805b0725a1334b68e9dd8a5b685d791
GiTurki <turkidzh@gmail.com>
2021-10-30 10:43:24 +0200

    extracted app to a new file and added supertest dependency Co-authored-by: Rubén Jorajuría <rubenjl@users.noreply.github.com>

---
+++ api/src/app.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response, Router } from "express";
+import morgan from "morgan";
+
+const app: Express = express();
+
+// Shows request log on terminal
+// https://github.com/expressjs/morgan
+app.use(morgan("dev"));
+
+// Parses incoming requests with JSON payloads
+// http://expressjs.com/es/api.html#express.json
+app.use(express.json());
+
+// Parses incoming requests with urlencoded payloads
+// http://expressjs.com/es/api.html#express.urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+const port: string = process.env.PORT || "3000";
+
+// Assigns setting name to value
+// http://expressjs.com/es/api.html#app.set
+app.set("port", port);
+
+export default app;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,28 +1,7 @@
 import http, { Server } from "http";
-import express, { Express, Request, Response, Router } from "express";
-import morgan from "morgan";
-
-const app: Express = express();
-const router: Router = express.Router();
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(morgan("dev"));
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json());
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
+import app from "app";
 
 const port: string = process.env.PORT || "3000";
-
-// Assigns setting name to value
-// http://expressjs.com/es/api.html#app.set
-app.set("port", port);
-
 const server: Server = http.createServer(app);
 
 server.listen(port);
