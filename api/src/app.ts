84ac491fb6f18dda456c3c8c910179e82f4b77c3
Javier Rodríguez <javitafalla95@gmail.com>
2021-10-30 10:37:54 +0200

    refactor app express config

---
+++ api/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+
+export const app: Express = express();
+// Shows request log on terminal
+// https://github.com/expressjs/morgan
+app.use(morgan("dev"));
+
+// Parses incoming requests with JSON payloads
+// http://expressjs.com/es/api.html#express.json
+app.use(express.json());
+
+// Parses incoming requests with urlencoded payloads
+// http://expressjs.com/es/api.html#express.urlencoded
+app.use(express.urlencoded({ extended: false }));
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,6 @@
 import http, { Server } from "http";
-import express, { Express, Request, Response } from "express";
-import morgan from "morgan";
-
-const app: Express = express();
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(morgan("dev"));
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json());
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
+import { Request, Response } from "express";
+import { app } from "./app";
 
 const port: string = process.env.PORT || "3000";
 
