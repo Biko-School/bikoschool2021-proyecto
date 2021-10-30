2f708f86a6a2afb0eb076d9144f320e620555ffb
Javier Rodríguez <javitafalla95@gmail.com>
2021-10-30 12:43:19 +0200

    test refactor

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -3,25 +3,13 @@ import request from "supertest";
 import { response } from "express";
 
 describe("/api/memes", () => {
-  it("Endpoint exist", (done) => {
-    request(app).get("/api/memes").expect(200, done);
-  });
-  it("should be array", (done) => {
-    request(app)
-      .get("/api/memes")
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toBeInstanceOf(Array);
-        done();
-      });
-  });
   it("should be array of 50", (done) => {
     request(app)
       .get("/api/memes")
       .expect(200)
       .then((response) => {
-        expect(response.body.length).toEqual(50);
-
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body).toHaveLength(50);
         done();
       });
   });
