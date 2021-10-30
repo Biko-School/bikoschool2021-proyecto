e4fd94416a9b90883fdc95542afbe98a57124f40
Javier Rodríguez <javitafalla95@gmail.com>
2021-10-30 12:29:39 +0200

    test: should be array of 50 ok

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,5 +3,9 @@ import { Router } from "express";
 export const routes = Router();
 
 routes.get("/memes", (req, res) => {
-  res.status(200).json([]);
+  const memes = [];
+  for (let index = 0; index < 50; index++) {
+    memes.push(`${index}`);
+  }
+  res.status(200).json(memes);
 });
