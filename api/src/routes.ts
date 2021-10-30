a6c6155f47382f768dbc2dabcfd7026b022b297e
Maciker <ikermacaya@gmail.com>
2021-10-30 13:33:22 +0200

    Devuelve los 50 primeros memes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express"
+import low from "lowdb"
+import FileSync from 'lowdb/adapters/FileSync'
+
+
+interface DataBaseSchema {
+    memes: Meme[]
+}
+
+interface Meme {}
+
+const adapter = new FileSync<DataBaseSchema>('./db/db.json')
+const db = low(adapter)
 
 export const routes = Router()
 
 routes.get('/memes', function (req, res) {
-    res.status(200).json([]);
+    const memes = db.get('memes').sortBy("import_datetime").take(50).value()
+    
+    res.status(200).json(memes);
 });
 
 export default routes;
