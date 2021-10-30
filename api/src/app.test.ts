8a6dcd24e7c476da476dd8cbaafca0df25d4b97c
Maciker <ikermacaya@gmail.com>
2021-10-30 10:50:15 +0200

    Test: Existe el endpoint /api/memes

---
+++ api/src/app.test.ts
@@ -0,0 +1,9 @@
+import app from "./app"
+import request from "supertest"
+
+describe('/api/memes', () => {
+    it("existe el endpoint"), (done) => {
+        request(app).get('/api/memes').expect(200, done);
+    }
+
+})
