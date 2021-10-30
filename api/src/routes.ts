1dbcb60ea0089addbf2b185bf9d4779bbc8d9ee1
GiTurki <turkidzh@gmail.com>
2021-10-30 12:22:33 +0200

    Test: verde

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,7 +3,7 @@ import express, { Express, Request, Response, Router } from "express";
 const routes = express.Router();
 
 routes.get("/memes", (req: Request, res: Response) => {
-  res.send([]);
+  res.send(new Array(50));
 });
 
 export default routes;
