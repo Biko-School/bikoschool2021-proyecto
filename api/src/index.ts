a5cac295c5e35bc7ef64ec495f92ebba01773f14
Silvaob <silvia_ob14@hotmail.com>
2021-10-30 10:42:34 +0200

    refactor express file and add supertest dependencie

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,36 +1,7 @@
 import http, { Server } from "http";
-import express, { Express, Request, Response } from "express";
-import morgan from "morgan";
-
-const app: Express = express();
-
-// Shows request log on terminal
-// https://github.com/expressjs/morgan
-app.use(morgan("dev"));
-
-app.get("/memes", function (req, res) {
-  res.send("this is memes");
-});
-
-// Parses incoming requests with JSON payloads
-// http://expressjs.com/es/api.html#express.json
-app.use(express.json());
-
-// Parses incoming requests with urlencoded payloads
-// http://expressjs.com/es/api.html#express.urlencoded
-app.use(express.urlencoded({ extended: false }));
-
-// Routes every path
-// http://expressjs.com/es/api.html#app.use
-app.use("/", (req: Request, res: Response) => {
-  res.json({ data: "index!" });
-});
+import { app } from "./express";
 
 const port: string = process.env.PORT || "3000";
-
-// Assigns setting name to value
-// http://expressjs.com/es/api.html#app.set
-app.set("port", port);
 
 const server: Server = http.createServer(app);
 
