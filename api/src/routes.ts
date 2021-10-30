a1655bb492687a0de2d4feaf428d59f343578921
Ibarra <aitoribarra@tutanota.com>
2021-10-30 13:39:49 +0200

    add: lowdb implemented

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 
+import low from "lowdb";
+import FileSync from "lowdb/adapters/FileSync";
+
 export const routes = Router();
 
+interface DbSchema {
+  memes: Meme[];
+}
+
+interface Meme {}
+
+const adapter = new FileSync<DbSchema>("./db/db.json");
+
+const db = low(adapter);
+
 routes.get("/memes", (req, res) => {
-  const memes = [];
-  for (let index = 0; index < 50; index++) {
-    memes.push(`${index}`);
-  }
+  const memes = db.get("memes").take(50).value();
   res.status(200).json(memes);
 });
