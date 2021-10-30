d83864578b40af4df2a0a84cbfae75f4c6e08da3
Endika <endikaorve@gmail.com>
2021-10-30 13:22:25 +0200

    WIP: Added lowdb

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,7 +1,19 @@
 import { Router, NextFunction, Request, Response } from 'express';
+import low from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
 
 export const routes = Router();
 
+interface DatabaseSchema {
+  memes: Meme[];
+}
+
+interface Meme {}
+
+const adapter = new FileSync<DatabaseSchema>('./db/db.json');
+const db = low(adapter);
+
 routes.get('/memes', (req: Request, res: Response) => {
-  res.status(200).json([{ data: 'Endika' }, { data: 'Carlos' }]);
+  const memes = db.get('memes').take(50).value();
+  res.status(200).json(memes);
 });
