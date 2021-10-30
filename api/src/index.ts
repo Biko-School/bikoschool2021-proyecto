575e550196f5b3d85307ab5a05861937905e1f1e
Endika <endikaorve@gmail.com>
2021-10-30 10:21:54 +0200

    Clear API example endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,61 +17,6 @@ app.use(express.json());
 // http://expressjs.com/es/api.html#express.urlencoded
 app.use(express.urlencoded({ extended: false }));
 
-//// ROUTES ////
-
-// Check session
-const checkSession = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-
-  if (true) { // ## TODO: Check Session
-    console.log('checkSession')
-    next()
-  } else {
-    res.sendStatus(401);
-  }
-};
-
-app.use("/users", checkSession)
-
-// Users CRUD
-app.get("/users", (req: Request, res: Response) => {
-  res.status(200).json([{ data: "Endika" },{ data: "Carlos" }]); // Status 200 default
-});
-
-app.get("/users/:id", (req: Request, res: Response) => {
-  res.json(`User with ID ${req.params.id}: Endika`);
-});
-
-app.post("/users", (req: Request, res: Response) => {
-  console.log('Create user')
-  res.json(req.body);
-});
-
-app.put("/users", (req: Request, res: Response) => {
-  console.log('Update user')
-  res.json(req.body);
-});
-
-app.delete("/users", (req: Request, res: Response) => {
-  console.log('Delete user')
-  res.json(req.body);
-});
-
-app.get("/errors/users", (req: Request, res: Response, next: NextFunction) => {
-  next(new Error('Users Error'))
-});
-
-// Routes every path
-// http://expressjs.com/es/api.html#app.use
-//app.use("/", (req: Request, res: Response) => {
-//  res.json({ data: "index!" });
-//});
-
-// 404
-app.use('*', (req: Request, res: Response) => {
-  res.json('no-controller');
-});
-
 const port: string = process.env.PORT || "3000";
 
 // Assigns setting name to value
