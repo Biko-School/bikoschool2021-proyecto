0755dacdf8b770686c542dd0c71f19718859783b
Endika <endikaorve@gmail.com>
2021-10-30 10:49:56 +0200

    Test: Endpoint /api/memes exists

---
+++ api/src/config/app.test.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+import request from "supertest";
+
+describe("/api/memes", () => {
+  it("endpoint exists", (done) => {
+    request(app).get("api/memes").expect(200, done);
+  });
+});
