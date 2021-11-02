a1a865bbc59e424ae8f668b138b00e82b8fdeac4
Javier Rodríguez <javitafalla95@gmail.com>
2021-11-02 18:24:46 +0100

    test: "should be array of 50" fixtures DB

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,6 +1,17 @@
-import { app } from "./app";
+import { createApp } from "./app";
 import request from "supertest";
 import { response } from "express";
+import low from "lowdb";
+import Memory from "lowdb/adapters/Memory";
+import { DbSchema } from "./DbSchema";
+import dbData from "./fixtures/db.json";
+
+const adapter = new Memory<DbSchema>("./fixtures/db.json");
+const db = low(adapter);
+
+db.defaults(dbData).write();
+
+const app = createApp(db);
 
 describe("/api/memes", () => {
   it("should be array of 50", (done) => {
