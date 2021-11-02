2534429d26d30bb52b190cdf72afcddbda31e3f7
Javier Rodríguez <javitafalla95@gmail.com>
2021-11-02 18:04:34 +0100

    fix: routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,20 +3,11 @@ import low from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
 import { DbSchema } from "./DbSchema";
 
-// export const routes = Router();
-
-// const adapter = new FileSync<DbSchema>("./db/db.json");
-// const db = low(adapter);
-
-// routes.get("/memes", (req, res) => {
-//   const memes = db.get("memes").take(50).value();
-//   res.status(200).json(memes);
-// });
-
 export function getRoutes(db: low.LowdbSync<DbSchema>) {
   const routes = Router();
   routes.get("/memes", (req, res) => {
     const memes = db.get("memes").take(50).value();
     res.status(200).json(memes);
   });
+  return routes;
 }
