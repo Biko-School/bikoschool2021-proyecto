44c44b13a899e587eef5447c2e5d74bfafd2476a
Endika <endikaorve@gmail.com>
2021-11-05 17:42:18 +0100

    Fix: Deshabilitar handler para que no interfiera con la petición real

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,8 +1,8 @@
-import { rest } from 'msw';
-import gifs from '../gif.json';
+// import { rest } from 'msw';
+// import gifs from '../gif.json';
 
 export const handlers = [
-  rest.get('http://api.bikoschool.dev/gifs', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(gifs));
-  }),
+  //   rest.get('http://api.bikoschool.dev/gifs', (req, res, ctx) => {
+  //     return res(ctx.status(200), ctx.json(gifs));
+  //   }),
 ];
