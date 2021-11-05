5e185c6df77614a02bde453ee6dbe78cf1a74004
rubenjl <jorajuria.ruben@gmail.com>
2021-11-05 19:19:19 +0100

    test: actualiza test para cubrir llamada a api modificada

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,40 +2,99 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Muestra el meme Happy Birthday", async () => {
+test("Muestra el meme Movie Brazil GIF by MOODMAN", async () => {
   render(<App />);
-  const meme = screen.getByRole("img", {
-    name: "Happy Birthday",
+  const meme = await screen.findByRole("img", {
+    name: "Movie Brazil GIF by MOODMAN",
   });
 
   expect(meme).toBeInTheDocument();
   expect(meme).toHaveAttribute(
     "src",
-    "https://i.giphy.com/media/H3kNUxf6NubwitJuu6/giphy.webp"
+    "https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
   );
 });
-
 test("Muestra tres memes", async () => {
   render(<App />);
   const memes = [
     {
-      title: "Happy Birthday",
-      src: "https://i.giphy.com/media/H3kNUxf6NubwitJuu6/giphy.webp",
+      "id": "YleuWir5NTNVXkflSp",
+      "type": "gif",
+      "slug": "moodman-movie-brazil-the-YleuWir5NTNVXkflSp",
+      "giphyUrl": "https://giphy.com/gifs/moodman-movie-brazil-the-YleuWir5NTNVXkflSp",
+      "title": "Movie Brazil GIF by MOODMAN",
+      "source_tld": "",
+      "source_post_url": "",
+      "import_datetime": "2020-08-20 02:24:22",
+      "username": "",
+      "images": {
+        "original": {
+          "width": "480",
+          "height": "269",
+          "url": "https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
+        },
+        "small": {
+          "width": "200",
+          "height": "112",
+          "url": "https://media4.giphy.com/media/YleuWir5NTNVXkflSp/200w.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=200w.gif"
+        }
+      },
+      "tags": ["#movie", "#brazil", "#brazil the movie"]
     },
     {
-      title: "Magical Mischief",
-      src: "https://media2.giphy.com/media/55d4cDB5zXccQlA8Ig/giphy.webp",
+      "id": "J6OQEgOUNOU5BWfjFj",
+      "type": "gif",
+      "slug": "moodman-J6OQEgOUNOU5BWfjFj",
+      "giphyUrl": "https://giphy.com/gifs/moodman-J6OQEgOUNOU5BWfjFj",
+      "title": "Dance Dancing GIF by MOODMAN",
+      "source_tld": "",
+      "source_post_url": "",
+      "import_datetime": "2020-08-26 22:51:59",
+      "username": "",
+      "images": {
+        "original": {
+          "width": "480",
+          "height": "402",
+          "url": "https://media3.giphy.com/media/J6OQEgOUNOU5BWfjFj/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
+        },
+        "small": {
+          "width": "200",
+          "height": "168",
+          "url": "https://media3.giphy.com/media/J6OQEgOUNOU5BWfjFj/200w.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=200w.gif"
+        }
+      },
+      "tags": ["#dance moves", "#dance", "#dancing", "#dancin"]
     },
     {
-      title: "Mayoral debate",
-      src: "https://media4.giphy.com/media/DG03l4Yfn2Iz52WFlT/giphy.webp",
-    },
+      "id": "l5DePfMmB09ZVkh3Af",
+      "type": "gif",
+      "slug": "moodman-funny-gif-l5DePfMmB09ZVkh3Af",
+      "giphyUrl": "https://giphy.com/gifs/moodman-funny-gif-l5DePfMmB09ZVkh3Af",
+      "title": "Funny Gif Lol GIF by MOODMAN",
+      "source_tld": "",
+      "source_post_url": "",
+      "import_datetime": "2020-08-26 22:20:35",
+      "username": "",
+      "images": {
+        "original": {
+          "width": "379",
+          "height": "498",
+          "url": "https://media1.giphy.com/media/l5DePfMmB09ZVkh3Af/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
+        },
+        "small": {
+          "width": "200",
+          "height": "263",
+          "url": "https://media1.giphy.com/media/l5DePfMmB09ZVkh3Af/200w.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=200w.gif"
+        }
+      },
+      "tags": ["#funny", "#hilarious", "#lol", "#funny gif"]
+    }
   ];
-  memes.forEach((meme) => {
-    const memeElement = screen.getByRole("img", {
+  memes.forEach(async (meme) => {
+    const memeElement = await screen.findByRole("img", {
       name: meme.title,
     });
     expect(memeElement).toBeInTheDocument();
-    expect(memeElement).toHaveAttribute("src", meme.src);
+    expect(memeElement).toHaveAttribute("src", meme.images.original.url);
   });
 });
