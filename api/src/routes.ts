cb27dd1ac449760602ad756b5eeedd90f0824221
rocio0504 <rocioarp2@gmail.com>
2021-11-05 17:24:40 +0100

    Add fields in route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -4,7 +4,9 @@ export const routes = Router();
 
 routes.get('/memes', function (req: Request, res: Response) {
     const db = req.context.db;
-    res.status(200).json(db.get('memes').take(50).value())
+    const memes = db.get('memes').take(50).value()
+    const cleanMemes = memes.map(({id, giphyUrl, tags, title }) => ({ id, name: title, tags, image: giphyUrl}));
+    res.status(200).json(cleanMemes);
 })
 
 routes.get('/memes/:memeId', function (req: Request, res: Response) {
@@ -13,5 +15,5 @@ routes.get('/memes/:memeId', function (req: Request, res: Response) {
     const db = req.context.db;
     const meme = db.get('memes').find({ id: memeId }).value()
     const { id, title: name, tags, giphyUrl: image } = meme
-    res.status(200).json({id, name,tags,image})
+    res.status(200).json({id, name, tags, image})
 })
