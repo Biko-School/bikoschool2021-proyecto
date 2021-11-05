42620e2ba12309728e194d9195768b8b218b9e9f
Yaiza Alvarez <yaialvarez13@gmail.com>
2021-11-05 20:33:33 +0100

    Diseño casi terminado

--- front/src/components/MemesList.tsx
+++ front/src/components/MemesList.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { api } from "../api";
+import logo from "../images/Logo.png";
+import arrow from "../images/Arrow.png";
+import lens from "../images/Lens.png";
 
 export interface Meme {
   title: string;
@@ -12,50 +15,64 @@ function sortMemeByDate(memes: Meme[]) {
   return memes.sort((a, b) => (a.date < b.date ? -1 : 1));
 }
 
-function chooseFiftyMemes(memes: Meme[], n: number) {
-  return memes.slice(0, n);
-}
+// function chooseFiftyMemes(memes: Meme[], n: number) {
+//   return memes.slice(0, n);
+// }
 
-function memesMapping(memesJSON:any) {
-  const memesData = memesJSON.map((meme:any) => {
+function memesMapping(memesJSON: any) {
+  const memesData = memesJSON.map((meme: any) => {
     return {
-      titles : meme.title,
-      tags : meme.tags,
-      date : meme.import_datetime,
-      img : meme.images.original.url 
-    }
-  })
-  return memesData
+      titles: meme.title,
+      tags: meme.tags,
+      date: meme.import_datetime,
+      img: meme.images.original.url,
+    };
+  });
+  return memesData;
 }
 
 export const MemessList: React.FC = () => {
   const [memes, setMemes] = useState<Meme[]>([]);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     (async () => {
       const memesJSON = await api.memes();
-      const memesJSONmapped = memesMapping(memesJSON)
+      const memesJSONmapped = memesMapping(memesJSON);
       const memesSorted = sortMemeByDate(memesJSONmapped);
-      const fiftyMemesSorted = chooseFiftyMemes(memesSorted, 50);
-      setMemes(fiftyMemesSorted);
+      setMemes(memesSorted);
     })();
   }, []);
 
   return (
     <div className="App">
-      <div className="App__AppTitle">GIFFAFFINITY</div>
+      <div className="App__AppTitle">
+        <img src={logo} alt="logo" width="55.88" height="61.75" />
+        <div className="App__AppTitle--Title">GIFFAFFINITY</div>
+      </div>
       <div className="App__SearchBar">
-        {/* <div onChange={(event) => setTag(event.target.value)} /> */}
+        <input
+          className="App__SearchBar--bar"
+          onChange={(event) => setName(event.target.value)}
+          value={name}
+        />
+        <div className="App__SearchBar--button">
+          <img src={lens} alt="Lens" width="25px" height="25px" />
+        </div>
       </div>
       <div className="App__SectionTitle">
-        Los giffs más trendings del momento
+        <img src={arrow} alt="logo" width="25" height="25" />
+        <div className="App__SectionTitle--Title">
+          Los giffs más trendings del momento
+        </div>
       </div>
-      <div className="App__GridGifs"></div>
-      {/* {memes.map((meme) => (
+      <div className="App__GridGifs">
+        {memes.map((meme) => (
           <div key={meme.img}>
-            <img src={meme.img} alt={meme.title} width="50%" height="50%"></img>
+            <img src={meme.img} alt={meme.title} width="25%" height="25%"></img>
           </div>
-        ))} */}
+        ))}
+      </div>
     </div>
   );
 };
