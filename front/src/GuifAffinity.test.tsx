e7c538ccf00c4d07f323f8e191343e7985f9cc77
jartigag <jartigag@pm.me>
2021-11-05 20:25:09 +0100

    fix test

--- front/src/GuifAffinity.test.tsx
+++ front/src/GuifAffinity.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import GuifAffinity from './GuifAffinity';
+import GuifCard from './components/GuiffCard';
 
 describe('GuifAffinity', () => {
-  it('shows a guif', () => {
+  it('shows the alt tag of a guif', () => {
     // Arrange
 
     // Act
-    render(<GuifAffinity />)
+    render(<GuifCard />)
 
     // Assert
     expect(screen.getByAltText(/guif/i)).toBeInTheDocument()
