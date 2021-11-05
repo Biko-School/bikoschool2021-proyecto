2486fc77976313cec025f40f9fb62d8e8bba8048
Endika <endikaorve@gmail.com>
2021-11-05 16:59:11 +0100

    Test: endpoint devuelve elementos de tipo Meme

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,9 +1,10 @@
 import { getApp } from './app';
 import request from 'supertest';
+import dbData from './fixtures/db.json';
 import low from 'lowdb';
 import Memory from 'lowdb/adapters/Memory';
 import { DatabaseSchema } from './DatabaseSchema';
-import dbData from './fixtures/db.json';
+import { Meme } from './Meme';
 
 describe('/api/memes', () => {
   let app;
@@ -17,11 +18,11 @@ describe('/api/memes', () => {
     app = getApp(db);
   });
 
-  it('endpoint exists', (done) => {
+  it('endpoint existe', (done) => {
     request(app).get('/api/memes').expect(200, done);
   });
 
-  it('endpoint returns array', (done) => {
+  it('endpoint devuelve array', (done) => {
     request(app)
       .get('/api/memes')
       .end((err, res) => {
@@ -31,12 +32,41 @@ describe('/api/memes', () => {
       });
   });
 
-  it('endpoint returns 50 items', (done) => {
+  it('endpoint devuelve 50 elementos', (done) => {
     request(app)
       .get('/api/memes')
       .end((err, res) => {
         expect(res.body.length).toBe(50);
         expect(res.body).toHaveLength(50);
+        done();
+      });
+  });
+
+  it('endpoint devuelve elementos de tipo Meme', (done) => {
+    request(app)
+      .get('/api/memes')
+      .end((err, res) => {
+        const testMeme: Meme = {
+          id: 'testID',
+          url: 'testURL',
+          title: 'testTitle',
+        };
+        const firstMeme = res.body[0];
+
+        expect(Object.keys(firstMeme).length).toEqual(
+          Object.keys(testMeme).length
+        );
+        Object.keys(testMeme).forEach((key) => {
+          expect(firstMeme).toHaveProperty(key);
+          expect(typeof firstMeme[key]).toEqual(typeof testMeme[key]);
+        });
+
+        //expect(firstMeme).toHaveProperty('id');
+        //expect(typeof firstMeme.id).toEqual('string');
+        //expect(firstMeme).toHaveProperty('url');
+        //expect(typeof firstMeme.url).toEqual('string');
+        //expect(firstMeme).toHaveProperty('title');
+        //expect(typeof firstMeme.title).toEqual('string');
         done();
       });
   });
