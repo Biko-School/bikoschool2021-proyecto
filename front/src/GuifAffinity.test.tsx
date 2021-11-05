3945a4e208baca4538423b4ddd1b7cf4eb465fef
jartigag <jartigag@pm.me>
2021-11-05 20:32:24 +0100

    test: it shows an image

--- front/src/GuifAffinity.test.tsx
+++ front/src/GuifAffinity.test.tsx
@@ -11,4 +11,14 @@ describe('GuifAffinity', () => {
     // Assert
     expect(screen.getByAltText(/guif/i)).toBeInTheDocument()
   })
+  it('shows an image', () => {
+    // Arrange
+
+    // Act
+    render(<GuifCard />)
+    const image = screen.getByAltText(/guif/i);
+
+    // Assert
+    expect(image).toContain('https://media4.giphy.com/media');
+  })
 })
