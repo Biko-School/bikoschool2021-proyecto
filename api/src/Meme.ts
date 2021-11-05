c18f4c4e32de056499c75e2a3d0fa2c3d0947b0c
Endika <endikaorve@gmail.com>
2021-11-05 17:03:44 +0100

    Feat: /api/memes devuelve 50 elementos de tipo Meme

---
+++ api/src/Meme.ts
@@ -0,0 +1,5 @@
+export interface Meme {
+  id: string;
+  url: string;
+  title: string;
+}
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,18 @@
-import { Router, NextFunction, Request, Response } from 'express';
-import { LowdbSync } from 'lowdb';
-import { DatabaseSchema } from 'DatabaseSchema';
+import { Router, Request, Response } from 'express';
+import { DatabaseMeme } from './DatabaseSchema';
+import { Meme } from './Meme';
 
 export const routes = Router();
 
 routes.get('/memes', (req: Request, res: Response) => {
   const db = req.context.db;
-  const memes = db.get('memes').take(50).value();
+  const DBMemes: DatabaseMeme[] = db.get('memes').take(50).value();
+  const memes: Meme[] = DBMemes.map((DBMeme: DatabaseMeme) => {
+    return {
+      id: DBMeme.id,
+      url: DBMeme.images.original.url,
+      title: DBMeme.title,
+    };
+  });
   res.status(200).json(memes);
 });
