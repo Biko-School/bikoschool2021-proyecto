d030d6e8bb86533e04886043cb79bb27d61c2a39
Ibarra <aitoribarra@tutanota.com>
2021-11-12 19:52:14 +0100

    refactor: implements hexagonal arch front

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./ui/App";
 
 if (process.env.NODE_ENV === "development") {
   const { worker } = require("./mocks/browser");
--- front/src/ui/App.tsx
+++ front/src/ui/App.tsx
@@ -1,31 +1,7 @@
 import React, { useState, useEffect } from "react";
-interface Data {
-  id: string;
-  type: string;
-  slug: string;
-  giphyUrl: string;
-  title: string;
-  source_tld: string;
-  source_post_url: string;
-  import_datetime: string;
-  username: string;
-  images: {
-    original: {
-      width: string;
-      height: string;
-      url: string;
-    };
-    small: {
-      width: string;
-      height: string;
-      url: string;
-    };
-  };
-  tags: string[];
-}
-
+import { Meme } from "../core/domain/Meme";
 function App() {
-  const [memesData, setMemesData] = useState<Data[]>([]);
+  const [memesData, setMemesData] = useState<Meme[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:3010/api/memes", { mode: "no-cors" })
