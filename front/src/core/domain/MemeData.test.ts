c3c0122d510c4d69cf8e1773081ba1f3b3d2bac3
jaime <jaimegonicarnicero@hotmail.com>
2021-11-13 10:00:09 +0100

    Refactor: remove classes and create databased-structured meme data INTERFACES

--- front/src/core/domain/MemeData.test.ts
+++ front/src/core/domain/MemeData.test.ts
@@ -1,16 +1,24 @@
-import { MemeData } from "./MemeData";
-import memes from '../../../../material/db.json'
+import { StoredMemeData } from "./MemeData";
+import db from '../../../../material/db.json'
 
 describe('Meme', () => {
 
     it('Has meme values', () => {
-        const meme = new MemeData(memes[0])
 
-        expect(meme.id).toBe("YleuWir5NTNVXkflSp")
-        expect(meme.slug).toBe("moodman-movie-brazil-the-YleuWir5NTNVXkflSp")
-        expect(meme.giphyUrl).toBe("https://giphy.com/gifs/moodman-movie-brazil-the-YleuWir5NTNVXkflSp")
-        expect(meme.title).toBe("Movie Brazil GIF by MOODMAN")
+        const databaseStoredMemeData : StoredMemeData =db.memes[0]
 
+
+        expect(databaseStoredMemeData.id).toBe("YleuWir5NTNVXkflSp")
+        expect(databaseStoredMemeData.slug).toBe("moodman-movie-brazil-the-YleuWir5NTNVXkflSp")
+        expect(databaseStoredMemeData.giphyUrl).toBe("https://giphy.com/gifs/moodman-movie-brazil-the-YleuWir5NTNVXkflSp")
+        expect(databaseStoredMemeData.title).toBe("Movie Brazil GIF by MOODMAN")
+
+    })
+    it('Has original & small images Urls', ()=>{
+
+        const databaseStoredMemeData : StoredMemeData =db.memes[0]
+        expect(databaseStoredMemeData.images.original.url).toContain("be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789")
+        expect(databaseStoredMemeData.images.small.url).toContain("200w.gif")
     })
 
 })
