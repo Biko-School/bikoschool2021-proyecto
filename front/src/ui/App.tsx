dbd9c6087ca8682fdf0c73d3aadc9455fe66411f
Javier R <javitafalla95@gmail.com>
2021-11-13 14:19:27 +0100

    feat: adds styles to app component

--- front/src/ui/App.tsx
+++ front/src/ui/App.tsx
@@ -17,14 +17,37 @@ function App() {
   }
 
   return (
-    <div className="memes_grid">
-      {memesData.map((result) => (
-        <Meme
-          title={result.title}
-          id={result.id}
-          imageUrl={result.images.small.url}
+    <div className="app_container">
+      <div className="header">
+        <img src="/img/GuifaffinityLogo.svg" alt="Guifaffinity logo" />
+        <h1 className="header_title">GUIFAFFINITY</h1>
+      </div>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="¿Que quieres buscar? ¡Encuentralo!"
+          className="input_search"
         />
-      ))}
+        <div className="search_icon">
+          <img src="/img/searchIcon.svg" alt="Search" />
+        </div>
+      </div>
+
+      <div className="subtitle">
+        <img src="/img/arrow.svg" alt="Search" className="subtitle_logo" />
+        <div className="subtitle_text">Los guif más trendings del momento</div>
+      </div>
+
+      <div className="memes_grid">
+        {memesData.map((result) => (
+          <Meme
+            title={result.title}
+            id={result.id}
+            imageUrl={result.images.small.url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
