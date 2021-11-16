c78da98762c193455a32f7233ceda040298e6743
jartigag <jartigag@pm.me>
2021-11-16 18:11:29 +0100

    fix: first test

--- front/src/components/GuiffCard.tsx
+++ front/src/components/GuiffCard.tsx
@@ -1,13 +1,12 @@
-
 // import Tag from "./Tag";
 import React from "react";
 import { StoredMemeData } from "../core/domain/StoredMemeData";
 
-const GuiffCard:React.VFC<{meme:StoredMemeData}> = (props) => {
-  const{ meme } = props
+const GuiffCard: React.VFC<{ meme: StoredMemeData }> = (props) => {
+  const { meme } = props;
   return (
     <>
-      <img src={meme.images.original.url} alt="guif" />
+      <img src={meme.images.original.url} alt="guif" key={meme.id} />
       {/* <Tag /> */}
       {/* {<pre>{JSON.stringify(memes[0].images.original.url, null, "\t")}</pre>} */}
     </>
