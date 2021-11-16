11d2dde6b66ef68eb0589ebcb33b54fc4a0f212d
jartigag <jartigag@pm.me>
2021-11-16 20:08:48 +0100

    style: GuiffCard relative

--- front/src/ui/components/GuiffCard/GuiffCard.tsx
+++ front/src/ui/components/GuiffCard/GuiffCard.tsx
@@ -12,11 +12,11 @@ const GuiffCard: React.VFC<{ meme: StoredMemeData }> = (props) => {
   }
 
   return (
-    <div>
+    <div className="guif-card">
       <img
         src={meme.images.original.url}
         alt="guif"
-        className="guif-card"
+        className="guif-card__img"
         key={meme.id}
       />
       <span className="guif-card__joined-tags">{joinedTags}</span>
