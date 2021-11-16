3e5a62c8247c70014fd008e4cdaf11cd65c68960
jartigag <jartigag@pm.me>
2021-11-16 19:18:25 +0100

    refactor: renamed GuiffCard.css (and linter applied)

--- front/src/ui/components/GuiffCard/GuiffCard.tsx
+++ front/src/ui/components/GuiffCard/GuiffCard.tsx
@@ -1,20 +1,24 @@
 //import Tag from "../Tag/Tag"
 import React from "react";
 import { StoredMemeData } from "../../../core/domain/StoredMemeData";
-import './GuiffCardStyles.css'
+import "./GuiffCard.css";
 
 const GuiffCard: React.VFC<{ meme: StoredMemeData }> = (props) => {
   const { meme } = props;
-  let joinedTags : string = "";
+  let joinedTags: string = "";
 
-  for (let tag in meme.tags)
-    {
-      joinedTags += meme.tags[tag] + " ";
-    }
+  for (let tag in meme.tags) {
+    joinedTags += meme.tags[tag] + " ";
+  }
 
   return (
     <div>
-      <img src={meme.images.original.url} alt="guif"  className='guif-card' key={meme.id}/>
+      <img
+        src={meme.images.original.url}
+        alt="guif"
+        className="guif-card"
+        key={meme.id}
+      />
       <span className="guif-card__joined-tags">{joinedTags}</span>
       {/* <Tag /> */}
       {/* {<pre>{JSON.stringify(memes[0].images.original.url, null, "\t")}</pre>} */}
