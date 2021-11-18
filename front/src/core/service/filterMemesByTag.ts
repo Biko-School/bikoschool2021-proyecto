c0a285ce9574995950d0e4315e1257a559bc2081
Ibarra <aitoribarra@tutanota.com>
2021-11-18 18:20:25 +0100

    feat: should search by tag

--- front/src/core/service/filterMemesByTag.ts
+++ front/src/core/service/filterMemesByTag.ts
@@ -2,7 +2,15 @@ import { MemeType } from "../domain/MemeType";
 
 export const filterMemesByTag = (searchingText: string, memes: MemeType[]) => {
   const filteredMemes = memes.filter((meme) => {
-    return meme.tags.indexOf(`#${searchingText}`) > -1;
+    let counter: number = 0;
+    meme.tags.forEach((tag) => {
+      if (tag.toLowerCase().includes(searchingText.toLowerCase())) {
+        counter++;
+      }
+    });
+    return counter > 0;
+
+    // return meme.tags.indexOf(`#${searchingText}`) > -1;
   });
 
   return filteredMemes;
