c5623c68768bb2c8cb86b29a8a99d4623291cf4f
Ibarra <aitoribarra@tutanota.com>
2021-11-18 18:38:05 +0100

    feat: should return an array

---
+++ front/src/core/service/filterMemesByNameOrTag.ts
@@ -0,0 +1,7 @@
+import { MemeType } from "../domain/MemeType";
+
+const filterMemesByNameOrTags = (searchingText: string, memes: MemeType[]) => {
+  return memes;
+};
+
+export default filterMemesByNameOrTags;
