496984e8b2f37895932ad7255fdb77496151adda
Rubén <jorajuria.ruben@gmail.com>
2021-11-23 19:03:01 +0100

    Test: crea test rojo debería buscar un meme por su título

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 import testMemes from "./mocks/fixtures/test-memes"
@@ -13,3 +13,21 @@ test.concurrent.each(testMemes)('Muestra el meme #%#', async (meme) => {
   expect(memeElement).toBeInTheDocument();
   expect(memeElement).toHaveAttribute("src", meme.images.original.url);
 });
+
+test('Debería encontrar un meme por su título', async () => {
+  render(<App/>);
+  const inputSearch = screen.getByPlaceholderText('¿Que quieres buscar? ¡Encuentralo!');
+
+  expect(inputSearch).toBeInTheDocument();
+  
+  const testMeme = testMemes[0];
+
+  fireEvent.input(inputSearch, { target: { value: testMeme.title } });
+
+  const memeElement = await screen.findByRole("img", {
+    name: testMeme.title,
+  });
+  expect(memeElement).toBeInTheDocument();
+  expect(memeElement).toHaveAttribute("src", testMeme.images.original.url);
+  
+})
