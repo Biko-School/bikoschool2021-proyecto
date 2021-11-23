98aa0560ab527eb653cad180ceebf1a5e89fb423
Javier R <javitafalla95@gmail.com>
2021-11-23 20:19:49 +0100

    feat: fetchFilteredMemes

--- front/src/core/infrastructure/fetchMemes.ts
+++ front/src/core/infrastructure/fetchMemes.ts
@@ -1,11 +1,10 @@
 import { MemeType } from "../domain/MemeType";
 
-const fetchAllMemes = async () => {
+const fetchMemes = async () => {
   const response = await fetch("http://localhost:3010/api/memes");
   const memes: MemeType[] = await response.json();
-  console.log(memes);
 
   return memes;
 };
 
-export default fetchAllMemes;
+export default fetchMemes;
