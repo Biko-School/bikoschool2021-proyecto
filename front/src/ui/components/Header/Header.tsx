8a773319b7fcb953cace8ba11c972107da53c1d5
jartigag <jartigag@pm.me>
2021-11-23 19:53:42 +0100

    add Trending Title

--- front/src/ui/components/Header/Header.tsx
+++ front/src/ui/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Header.css";
 
-export const Header: React.VFC<{}> = ({}) => {
+export const Header: React.VFC<{}> = () => {
   return (
-    <div className="header-container">
-      {" "}
-      <img src="Logo.png" alt="" /> <h1>GUIFAFFINITY</h1>
-    </div>
+    <>
+      <div className="header-container">
+        <img className="logo" src="Logo.png" alt="Guifaffinity icon" />
+        <h1>GUIFAFFINITY</h1>
+      </div>
+      <div className="trending-title-container">
+        <img
+          className="trending-icon"
+          src="icon/trending.svg"
+          alt="trending icon"
+        />
+        <h2>Los guif más trendings del momento</h2>
+      </div>
+    </>
   );
 };
 
