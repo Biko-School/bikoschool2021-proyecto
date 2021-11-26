36d93eaf69b7493c2c3911e3e02369789d40e2c5
Maciker <ikermacaya@gmail.com>
2021-11-26 19:11:18 +0100

    Probando Husky

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,15 @@
-import './reset.css';
-import './App.css';
-import Home from '../src/screens/home'
-import Title from '../src/screens/title/title';
+import "./reset.css";
+import "./App.css";
+import Home from "../src/screens/home";
+import Title from "../src/screens/title/title";
 
 function App() {
- 
   return (
     <div className="container">
       <Title />
       <Home />
     </div>
-  )
-  }
+  );
+}
 
 export default App;
