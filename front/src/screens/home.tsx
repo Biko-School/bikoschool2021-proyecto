1973dcf5ec78030c38224ed5c585c48628cedf46
Maciker <ikermacaya@gmail.com>
2021-11-27 09:15:48 +0100

    Usando el linter

--- front/src/screens/home.tsx
+++ front/src/screens/home.tsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
-import MemeList from "../core/services/memeList"
-import Search from "../core/services/search"
-import "./home.css"
+import MemeList from "../core/services/memeList";
+import Search from "../core/services/search";
+import "./home.css";
 
-function Home(){
-    
-    const memes = MemeList()
+function Home() {
+  const memes = MemeList();
 
-    const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState("");
 
-    const filteredMemes = memes.filter(meme => meme.title.toLowerCase().includes(filter.toLowerCase()))
-    console.log(filteredMemes)
-    
-    return(
-        <>
-        <Search onFilter={setFilter}/>
-        
-        <div>
-            {
-            filteredMemes ? 
-            <div className="meme-parent-container">
-                <ul className="meme-container">{filteredMemes.map((meme)=> (
-                    <li>
-                        <img className="meme" src={meme.image} alt="meme"/>
-                    </li>
-                ))}
-                </ul>
-                
-            </div> : <span>No hay memes</span>
-            }
+  const filteredMemes = memes.filter((meme) =>
+    meme.title.toLowerCase().includes(filter.toLowerCase())
+  );
+  console.log(filteredMemes);
+
+  return (
+    <>
+      <Search onFilter={setFilter} />
+
+      <div>
+        {filteredMemes ? (
+          <div className="meme-parent-container">
+            <ul className="meme-container">
+              {filteredMemes.map((meme) => (
+                <li>
+                  <img className="meme" src={meme.image} alt="meme" />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <span>No hay memes</span>
+        )}
       </div>
-      </>
-    )
+    </>
+  );
 }
 
 export default Home;
