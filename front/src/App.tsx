9d48dbd9165a12500f1e017dd19846c24c9856e0
Endika <endikaorve@gmail.com>
2021-11-27 09:38:57 +0100

    Chore: Test Error 2

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Home } from "./components/Home";
+            import { Home } from "./components/Home";
 
 function App() {
+  console.log('Test Error')
   return <Home />;
 }
 
