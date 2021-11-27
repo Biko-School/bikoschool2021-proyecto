438aaa2518a82fc25d6009f5ca0acc48b8ea9ca3
GiTurki <turkidzh@gmail.com>
2021-11-27 10:42:59 +0100

    Chore: instala firebase CLI

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,6 +3,28 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA91NRv6TWBRQ5BHLtAGZ6rkRn0CEVDQt8",
+  authDomain: "turkiffinity.firebaseapp.com",
+  projectId: "turkiffinity",
+  storageBucket: "turkiffinity.appspot.com",
+  messagingSenderId: "629791527590",
+  appId: "1:629791527590:web:f71e6e55d00b26bc779a67",
+  measurementId: "G-8JTSVDLN2M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
