c0d03de29edad418dc13a30b0a5b07d836c57dd4
Yaiza Alvarez <yaialvarez13@gmail.com>
2021-11-27 10:34:31 +0100

    Modificaciones de prueba

--- front/public/mockServiceWorker.js
+++ front/public/mockServiceWorker.js
@@ -4,7 +4,7 @@
 /**
  * Mock Service Worker (0.35.0).
  * @see https://github.com/mswjs/msw
- * - Please do NOT modify this file..
+ * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
  */
 
