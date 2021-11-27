c898ef6ca47afe2993f6c50b4acb2ac4594ee8ad
merino95 <merino.100636@e.unavarra.es>
2021-11-27 11:15:03 +0100

    chamear el backend

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-if (process.env.NODE_ENV === "development") {
-  // const { worker } = require('./mocks/browser')
-  // worker.start()
-}
+// if (process.env.NODE_ENV === "development") {
+const { worker } = require("./mocks/browser");
+worker.start();
+//}
 
 ReactDOM.render(
   <React.StrictMode>
