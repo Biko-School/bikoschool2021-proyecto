c268e41b3da1cd35acc5cd94fc172e766ae98f4f
Endika <endikaorve@gmail.com>
2021-11-30 22:58:21 +0100

    Refactor: Limpia importaciones que no se usan

--- front/src/components/Home/Home.tsx
+++ front/src/components/Home/Home.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Home.css';
-import axios from 'axios';
 
 import { HomeHeader } from './components/HomeHeader';
 import { HomeMemesTitle } from './components/HomeMemesTitle';
