9bf20ab405057e6e12a2109bb5fff95660327426
jaime <jaimegonicarnicero@hotmail.com>
2021-11-30 18:04:17 +0100

    Refactor: create the HomePage and link it to App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,38 +1,9 @@
 import "./_reset.css";
 import "./App.css";
-import GuifContainer from "./ui/components/GuifContainer/GuifContainer";
-import Header from "./ui/components/Header/Header";
-import { useState, useEffect } from "react";
-import Meme from "./core/domain/Meme";
-import { fetchAllMemes } from "./core/service/ApiService";
+import {HomePage} from "./ui/views/home/HomePage"
 
 function App() {
-  const [memes, setMemes] = useState<Meme[]>([]);
-
-  const getMemes = async () => {
-    const response = await fetchAllMemes();
-    setMemes(response);
-  };
-
-  useEffect(() => {
-    getMemes();
-  }, []);
-
-  if (memes.length === 0) {
-    return (
-      <>
-        <Header />
-        <p>Cargando...</p>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Header />
-      <GuifContainer memes={memes} />
-    </>
-  );
+  return(<HomePage/>)
 }
 
 export default App;
