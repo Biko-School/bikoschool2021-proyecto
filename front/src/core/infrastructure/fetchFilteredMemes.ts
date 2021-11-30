cc083084da7ce4b832a43503ba5665cec02f1246
Ibarra <aitoribarra@tutanota.com>
2021-11-30 20:22:00 +0100

    feat: update fetchFilteredMemes

--- front/src/core/infrastructure/fetchFilteredMemes.ts
+++ front/src/core/infrastructure/fetchFilteredMemes.ts
@@ -2,8 +2,9 @@ import { MemeType } from "../domain/MemeType";
 
 const fetchFilteredMemes = async (searchText: string) => {
   if (searchText === "") return [];
+  const uriSearchText = encodeURIComponent(searchText);
   const response = await fetch(
-    `http://localhost:3010/api/memes/filter/${searchText}`
+    `http://localhost:3010/api/memes/filter/${uriSearchText}`
   );
   const memes: MemeType[] = await response.json();
 
