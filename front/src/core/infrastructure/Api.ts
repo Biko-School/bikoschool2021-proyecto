7e75f88e3f37639179501b37f09bc959794badc9
jartigag <jartigag@pm.me>
2021-12-09 20:08:01 +0100

    prettier

--- front/src/core/infrastructure/Api.ts
+++ front/src/core/infrastructure/Api.ts
@@ -1,51 +1,53 @@
-import Meme from "../domain/Meme";
-import MemeRepository from "../domain/MemeRepository";
+import Meme from '../domain/Meme';
+import MemeRepository from '../domain/MemeRepository';
 
 export const api: MemeRepository = {
-  getAllMemes: async (): Promise<Meme[]> => {
-    const response = await fetch("https://raw.githubusercontent.com/jartigag/bikoschool2021-proyecto/jaime-javi/api/memes.json");
-    const allMemesData: StoredMemeData[] = await response.json();
+    getAllMemes: async (): Promise<Meme[]> => {
+        const response = await fetch(
+            'https://raw.githubusercontent.com/jartigag/bikoschool2021-proyecto/jaime-javi/api/memes.json',
+        );
+        const allMemesData: StoredMemeData[] = await response.json();
 
-    let memeArray: Meme[] = [];
+        let memeArray: Meme[] = [];
 
-    for (let i in allMemesData) {
-      memeArray.push({
-        id: allMemesData[i].id,
-        title: allMemesData[i].title,
-        username: allMemesData[i].username,
-        tags: allMemesData[i].tags,
-        width: allMemesData[i].images.original.width,
-        height: allMemesData[i].images.original.height,
-        url: allMemesData[i].images.original.url,
-      });
-    }
-    return memeArray;
-  },
+        for (let i in allMemesData) {
+            memeArray.push({
+                id: allMemesData[i].id,
+                title: allMemesData[i].title,
+                username: allMemesData[i].username,
+                tags: allMemesData[i].tags,
+                width: allMemesData[i].images.original.width,
+                height: allMemesData[i].images.original.height,
+                url: allMemesData[i].images.original.url,
+            });
+        }
+        return memeArray;
+    },
 };
 
 export default api;
 
 export interface StoredMemeData {
-  id: string;
-  type: string;
-  slug: string;
-  giphyUrl: string;
-  title: string;
-  source_tld: string;
-  source_post_url: string;
-  import_datetime: string;
-  username: string;
-  images: dbImages;
-  tags: string[];
+    id: string;
+    type: string;
+    slug: string;
+    giphyUrl: string;
+    title: string;
+    source_tld: string;
+    source_post_url: string;
+    import_datetime: string;
+    username: string;
+    images: dbImages;
+    tags: string[];
 }
 
 export interface dbImages {
-  original: dbImagesDimensionsAndUrl;
-  small: dbImagesDimensionsAndUrl;
+    original: dbImagesDimensionsAndUrl;
+    small: dbImagesDimensionsAndUrl;
 }
 
 export interface dbImagesDimensionsAndUrl {
-  width: string;
-  height: string;
-  url: string;
+    width: string;
+    height: string;
+    url: string;
 }
